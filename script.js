2c22c6820c58768f0f3f62cc170d876b9065c5a0
bvu12 <brian.vu12@gmail.com>
2021-12-19 01:15:58 -0800

    completed calculator functionality with keyboard inputs

--- script.js
+++ script.js
@@ -36,8 +36,25 @@ let prevVal = "";
 let currVal = "";
 
 const ints = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+// For the display of the calculator
 const screen = document.querySelector(".console");
+screen.addEventListener('transitionend', function(e) {
+    // Small animation to let the user know their button has been pressed
+    removePlaying();
+});
+
 const MAX_DIGITS = 8;
+
+// For the keyboard functionality
+document.addEventListener('keydown', (event) => {
+    var name = event.key;
+    var code = event.code;
+
+    // Alert the key name and key code on keydown
+    // console.log(`Key pressed ${name} \r\n Key code value: ${code}`);
+    keyboardAction(name);
+});
 
 // Listener for integers
 for (let i = 0; i < ints.length; i++) {
@@ -65,6 +82,9 @@ for (let i = 0; i < ints.length; i++) {
     });
 }
 
+// The following buttons determine what happens when an arithmetic operation is pressed
+//  If there are valid entries to evaluate (e.g. a first value and a previous value are given), then it will be evaluated
+//  Otherwise, the calculator waits for two valid inputs 
 const plusBtn = document.querySelector("#plus");
 plusBtn.addEventListener("click", e => { 
     saveValueOrPerformOperation();
@@ -102,70 +122,76 @@ divideBtn.addEventListener("click", e => {
     floatState = true;    
 })
 
+// Square root is special - it does not need an a and b
+//  It will attempt to square root whatever is on the screen
 const sqrtBtn = document.querySelector("#sqrt");
 sqrtBtn.addEventListener("click", e => { 
     
-    // Square root is special - it does not need an a and b
     operationType = operationEnum.SQRT;
     floatState = true;    
     equalBtn.click();    
 })
 
+// The equal button also evaluates the expression
 const equalBtn = document.querySelector("#equals");
 equalBtn.addEventListener("click", e => {
-    let returnVal = applyFunction();
-    let returnValString = returnVal.toString();
+    removePlaying();
+    let returnVal = checkReturnVal(applyFunction());
 
-    // Truncate if necessary
-    if (returnValString.length > MAX_DIGITS) {
-        if (returnValString.includes(".")) {
-            returnVal = returnVal.toFixed(MAX_DIGITS-1);
-        } 
-        returnVal = returnVal.toString().substr(0,MAX_DIGITS);     
-    }   
-    
-    screen.textContent = returnVal; 
-    prevVal = returnVal;
-    currVal = "";
-    operationType = operationEnum.EQUALS;
+    if (returnVal != false) {    
+        screen.textContent = returnVal;
+        prevVal = returnVal;
+        currVal = "";
+        operationType = operationEnum.EQUALS;
+    }
 })
 
 // Reset the calculator
 const clearBtn = document.querySelector("#clear");
 clearBtn.addEventListener("click", e=> {
-    promptState = true;
-    floatState = false;
-
-    prevVal = "";
-    currVal = "";
-    operationType = "";
-    screen.textContent = prevVal;
+    location.reload();
 });
 
-const decimalBtn = document.querySelector("#decimal");
-decimalBtn.addEventListener("click", e=> {
-
-    // Only add a decimal if it does not already exist
-    if (!getScreenValue().includes(".")) {
-
-        // If blank, assume to mean "0." is desired
-        if (currVal == "") {
-            currVal = 0;
-        }
-
-        // Add the decimal
-        floatState = true;
-        currVal += ".";
+// Clear what is on the screen
+const clearEntryBtn = document.querySelector("#clear-entry");
+clearEntryBtn.addEventListener("click", e=> {
+    // If we most recently pressed "EQUALS", CE is equivalent to clear
+    if (operationType = operationEnum.EQUALS) {
+        clearBtn.click();
+    } else { // Otherwise, just clear the most recent input
+        currVal = "";
         screen.textContent = currVal;
     }
 
 });
 
+// Allow users to use decimals
+const decimalBtn = document.querySelector("#decimal");
+decimalBtn.addEventListener("click", e=> {
+
+    // If blank, assume to mean "0." is desired
+    if (currVal == "") {
+        screen.textContent = 0;
+    }
+
+    // Do not allow double decimal
+    if (!getScreenValue().includes(".")) {
+
+
+
+        // Add the decimal
+        floatState = true;
+        currVal += ".";
+        screen.textContent = currVal;
+    } 
+
+});
+
+// Change from pos to neg or vice-versa
 const plusMinusBtn = document.querySelector("#pm");
 plusMinusBtn.addEventListener("click", e=> {
     let screenValue = getScreenValue();
     
-    // Change from pos to neg or vice-versa
     if (screenValue.includes("-")) {
         screenValue = screenValue.substr(1,screenValue.length);
     } else {
@@ -182,8 +208,6 @@ plusMinusBtn.addEventListener("click", e=> {
     screen.textContent = screenValue;
 });
 
-// TODO: Move the percent and CE from the image
-
 // Based on the most recent operation, apply a function
 function applyFunction() {
     let a, b;
@@ -196,7 +220,7 @@ function applyFunction() {
         b = parseFloat(currVal);
     }
 
-    console.log("a: " + a + " b: " + b + " operation: " + getKeyByValue(operationEnum, operationType))
+    // console.log("a: " + a + " b: " + b + " operation: " + getKeyByValue(operationEnum, operationType))
 
     switch(operationType) {
         case operationEnum.ADD:
@@ -212,17 +236,19 @@ function applyFunction() {
             return a / b;
             break;
         case operationEnum.SQRT:
-            return Math.sqrt(parseFloat(getScreenValue()));
+            let val = Math.sqrt(parseFloat(getScreenValue()));
+            return isNaN(val) ? "err" : val;
         default:
-          // code block
+          return NaN;
       }
 }
 
 // EFFECTS: If we recently flushed, then we need to initialize/save prevVal
 //          Otherwise, we may need to evaluate the most recent operation to chain them
 function saveValueOrPerformOperation() {
-    // TODO: Flash to indicate that the arithmetic button has been pressed;
-    screen.textContent = "";
+    removePlaying();
+    screen.classList.add('playing');
+
        
     // If we recently flushed, then we need to initialize prevVal
     if (prevVal == "") {
@@ -231,10 +257,13 @@ function saveValueOrPerformOperation() {
     // If the previous operation was another arithmetic operation, we need to evaluate it
     //  Otherwise, we recently came from an equals... do nothing
     } else if (operationType != operationEnum.EQUALS){
-        // TODO: Similar to equals
-        prevVal = applyFunction().toString();
-        screen.textContent = prevVal; 
-        currVal = "";
+        let returnVal = checkReturnVal(applyFunction());
+
+        if (returnVal != false) {
+            prevVal = returnVal;
+            screen.textContent = prevVal; 
+            currVal = "";
+        }
     }
 }
 
@@ -248,3 +277,88 @@ function getKeyByValue(object, value) {
     return Object.keys(object).find(key => object[key] === value);
   }
 
+// Check what our calculator calculated
+function checkReturnVal(returnVal) {
+    // If it returns a valid number or "err"
+    if (!isNaN(returnVal) || returnVal == "err") {
+        let returnValString = returnVal.toString();
+
+        // Truncate if necessary
+        if (returnValString.length > MAX_DIGITS) {
+            if (returnValString.includes(".")) {
+                returnVal = returnVal.toFixed(MAX_DIGITS-1);
+            } 
+            returnVal = returnVal.toString().substr(0,MAX_DIGITS);     
+        }          
+        return returnVal;
+    // If it is not a number
+    } else {
+        return false
+    }
+}
+
+// Keyboard to action
+function keyboardAction(name) {
+    switch(name) {
+        case "0":
+            document.querySelector("#" + ints[0]).click();
+            break;
+        case "1":
+            document.querySelector("#" + ints[1]).click();
+            break;
+        case "2":
+            document.querySelector("#" + ints[2]).click();
+            break;
+        case "3":
+            document.querySelector("#" + ints[3]).click();
+            break;
+        case "4":
+            document.querySelector("#" + ints[4]).click();
+            break;
+        case "5":
+            document.querySelector("#" + ints[5]).click();
+            break;      
+        case "6":
+            document.querySelector("#" + ints[6]).click();
+            break;
+        case "7":
+            document.querySelector("#" + ints[7]).click();
+            break;
+        case "8":
+            document.querySelector("#" + ints[8]).click();
+            break;      
+        case "9":
+            document.querySelector("#" + ints[9]).click();
+            break;  
+        case "+":
+            plusBtn.click();
+            break;
+        case "-":
+            subtractBtn.click();
+            break;
+        case "/":
+            divideBtn.click();
+            break;
+        case "*":
+            multiplyBtn.click();
+            break; 
+        case "=":
+        case "Enter":
+            equalBtn.click();
+            break;   
+        case ".":
+            decimalBtn.click();
+            break;
+        default:
+            // Do nothing
+    }
+
+    
+}
+
+// Remove the playing transition class
+function removePlaying() {
+    if (screen.classList.contains("playing")) {
+        screen.classList.remove('playing');
+    }
+}
